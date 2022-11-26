719f59eecebdde9bc16c5a14c754c5a1f71a3d06
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2022-11-26 20:15:00 +0500

    changes of 26.11-7

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ class User(db.Model):
     __tablename__ = 'users'
     """Модель класса пользователи."""
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(36), unique=True, nullable=False)
+    password = db.Column(db.String(36), unique=True, nullable=False)
 
 
 class UserSchema(Schema):
