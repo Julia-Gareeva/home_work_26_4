a8aba117266f872d87521974368a5f94c9a15dae
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2022-11-26 19:06:55 +0500

    changes of 26.11-3

--- docker_config_ci.py
+++ docker_config_ci.py
@@ -1,6 +1,6 @@
 class Config(object):
     """Файл с настройками."""
-    SQLALCHEMY_DATABASE_URI = "postgresql://$DB_USER:$DB_PASSWORD@postgres/$DB_NAME"
+    SQLALCHEMY_DATABASE_URI = "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres/$POSTGRES_DB"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RESTX_JSON = {'ensure_ascii': False, 'indent': 4}
     DEBUG = True
