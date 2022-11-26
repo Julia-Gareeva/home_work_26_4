b5d0141771314e8877c3ef80fe1533e977a1033d
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2022-11-26 18:00:08 +0500

    changes of 26.11-1

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from default_config import Config
 from main import users_ns
 from model import db
 
-
 def create_app(config_object):
     app = Flask(__name__)
     app.config.from_object(config_object)
